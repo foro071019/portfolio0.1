febac597234e9b57675e5cdbbf3762f43af62eae
foro071019 <115169575+foro071019@users.noreply.github.com>
2023-12-16 20:28:18 +0800

    Add files via upload

---
+++ BIGFINAL/assets/script/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Your website content is now loaded
+  var preloader = document.querySelector(".preloader");
+  preloader.style.display = "none";
+});
