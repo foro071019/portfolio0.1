d6ab9344f661a07eff93ce7b7b610456b5c0a556
foro071019 <115169575+foro071019@users.noreply.github.com>
2023-12-16 20:33:49 +0800

    Delete BIGFINAL directory

--- BIGFINAL/assets/script/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Your website content is now loaded
-  var preloader = document.querySelector(".preloader");
-  preloader.style.display = "none";
-});
